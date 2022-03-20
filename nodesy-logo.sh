25838777f894206464b80deea21a7334bca68713
sowell-owen <65121631+sowell-owen@users.noreply.github.com>
2022-03-20 10:54:10 +0200

    Create nodesy-logo.sh

---
+++ nodesy-logo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BLUE='\033[0;34m'
+echo ""
+echo ""
+echo -e "$BLUE _   _           _      ____"
+echo -e "$BLUE| \ | | ___   __| | ___/ ___| _   _"
+echo -e "$BLUE|  \| |/ _ \ / _* |/ _ \___ \| | | |"
+echo -e "$BLUE| |\  | (_) | (_| |  __/___) | |_| |"
+echo -e "$BLUE|_| \_|\___/ \__._|\___|____/ \__. |"
+echo -e "$BLUE                              |___/"
+echo ""
+echo ""
+echo -e  "\033[1;32;5mTelegram: t.me/nodesy - node Community (RU)\033[0m"
+echo ""
+echo ""
+echo -e "\033[1;32;5mTwitter: @sowell_nodesy\033[0m"
+echo ""
+echo ""
